96147eebb8bbefa132357b614b8beaa3c8c8e193
rudolrandie <rudolrandie@gmail.com>
2019-10-05 18:52:02 -0700

    Added animations to player.
    Also fixed an issue where player would get stuck on the corners of tiles during jumps.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public CharacterController2D controller;
+	public Animator animator;
+	public Rigidbody2D m_Rigidbody2D;
 	float horizontalMove = 0f;
+	float verticalMove = 0f;
 	public float runSpeed = 40f;
 	bool jump = false;
 
@@ -19,17 +22,28 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		verticalMove = m_Rigidbody2D.velocity.y;
+		animator.SetFloat("H_Speed", Mathf.Abs(horizontalMove));
+		animator.SetFloat("V_Speed", verticalMove);
 
 		if (Input.GetButtonDown("Jump"))
 		{
 			jump = true;
 		}
+		Debug.Log("Current vertical velocity: " + m_Rigidbody2D.velocity.y);
+		if (verticalMove < 2.5f && verticalMove > -2.5f)
+		{
+			animator.SetBool("IsAerial", false);
+		} else
+		{
+			animator.SetBool("IsAerial", true);
+		}
     }
-
 	void FixedUpdate()
 	{
 		// Move our character
 		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
 		jump = false;
+		
 	}
 }
