1d60af392254f864f646adc7a42a7a738ac36f4a
megasha <jimnguyen123@gmail.com>
2019-10-06 03:52:25 -0700

    Added Background and parallax script

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class BackgroundManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject background;
+    [SerializeField] GameObject foreground;
+
+    [SerializeField] float backgroundXScale = 0;
+    [SerializeField] float backgroundYScale = 0;
+
+    [SerializeField] float foregroundXScale = 0;
+    [SerializeField] float foregroundYScale = 0;
+
+    [SerializeField] float backgroundXPosOffset = 0;
+    [SerializeField] float backgroundYPosOffset = 0;
+
+    [SerializeField] float foregroundXPosOffset = 0;
+    [SerializeField] float foregroundYPosOffset = 0;
+
+
+
+    private CinemachineVirtualCameraBase cam;
+    private Material backMat;
+    private Material foreMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCameraBase>();
+        backMat = background.GetComponent<Renderer>().material;
+        foreMat = foreground.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        background.transform.position = new Vector3(cam.transform.position.x + backgroundXPosOffset, cam.transform.position.y + backgroundYPosOffset, background.transform.position.z);
+        backMat.mainTextureOffset = new Vector2(cam.transform.position.x*backgroundXScale, cam.transform.position.y* backgroundYScale);
+
+        foreground.transform.position = new Vector3(cam.transform.position.x + foregroundXPosOffset, cam.transform.position.y + foregroundYPosOffset, foreground.transform.position.z);
+        foreMat.mainTextureOffset = new Vector2(cam.transform.position.x * foregroundXScale, cam.transform.position.y * foregroundYScale);
+    }
+}
